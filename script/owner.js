89f243ec565c187cb00fe3ed878d2b9d2f02a519
sunday <sunday@vm1.pingexample.com>
2018-08-30 14:59:20 +0100

    Complete HRDB/CTDB connector configuration

--- script/owner.js
+++ script/owner.js
@@ -1,8 +1,8 @@
-if(source !== "unassigned") {
+if (source !== "unassigned") {
   var ownerQry = openidm.query("managed/user", {
-    "_queryFilter" : "userName eq '" + source + "'"
+    "_queryFilter": "userName eq '" + source + "'"
   });
-  if(ownerQry.result.length === 1) {
-    ({ _ref: "managed/user/" + ownerQry.result[0]._id});
+  if (ownerQry.result.length === 1) {
+    ({ _ref : "managed/user/" + ownerQry.result[0]._id});
   }
 }
